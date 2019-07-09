1f5be4b4d396c855059388352ea6af881a0e0f03
Nicole Wong Yung Cheng <34061869+prophecy-blade@users.noreply.github.com>
2019-07-09 12:42:18 +0800

    Added fixed currency option

--- Extension_2 (Calculla)/Basic Calculator/currencies.js
+++ Extension_2 (Calculla)/Basic Calculator/currencies.js
@@ -16,5 +16,21 @@ var currenciesJSON = {
   INR: { name: 'Indian Rupee' },
   IDR: { name: 'Indonesian Rupiah' },
   ILS: { name: 'Israeli New Sheqel' },
-  JPY: { name: 'Japanese Yen' }
+  JPY: { name: 'Japanese Yen' },
+  MYR: { name: 'Malaysian Ringgit' },
+  MXN: { name: 'Mexican Peso' },
+  NZD: { name: 'New Zealand Dollar' },
+  NOK: { name: 'Norwegian Krone' },
+  PHP: { name: 'Philippine Peso' },
+  PLN: { name: 'Polish Zloty' },
+  RON: { name: 'Romanian New Leu' },
+  RUB: { name: 'Russian Rouble' },
+  SGD: { name: 'Singapore Dollar' },
+  ZAR: { name: 'South African Rand' },
+  KRW: { name: 'South Korean Won' },
+  SEK: { name: 'Swedish Krona' },
+  CHF: { name: 'Swiss Franc' },
+  THB: { name: 'Thai Baht' },
+  TRY: { name: 'Turkish Lira' },
+  USD: { name: 'United States Dollar' }
 };
