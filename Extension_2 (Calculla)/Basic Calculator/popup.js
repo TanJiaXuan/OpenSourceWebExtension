e8bf6637264ee16c651af30a75a9ba7c5ab98062
Nicole Wong Yung Cheng <34061869+prophecy-blade@users.noreply.github.com>
2019-07-09 11:35:03 +0800

    added add currency function

--- Extension_2 (Calculla)/Basic Calculator/popup.js
+++ Extension_2 (Calculla)/Basic Calculator/popup.js
@@ -31,3 +31,56 @@ function onPaste(event) {
 
   event.target.value = value;
 }
+
+function addCurrency(init) {
+  var input_value = document.createElement('input');
+  input_value.type = 'text';
+  input_value.id = 'value' + count_currencies;
+  input_value.name = count_currencies;
+  input_value.value = 'loading...';
+  input_value.setAttribute('maxlength', 12);
+  input_value.addEventListener('keyup', onValueChange, false);
+  input_value.addEventListener('paste', onPaste, false);
+  input_value.onkeypress = function() {
+    return onKeyPress(event);
+  };
+
+  var td_left = document.createElement('td');
+  td_left.setAttribute('class', 'paddingtd');
+  td_left.appendChild(input_value);
+
+  var select_currency = document.createElement('select');
+  select_currency.id = 'currency' + count_currencies;
+  select_currency.name = count_currencies;
+  select_currency.addEventListener('keypress', onCurrencyChange, false);
+  select_currency.addEventListener('change', onCurrencyChange, false);
+
+  for (var currency in currenciesJSON) {
+    var option = document.createElement('option');
+    option.value = currency;
+    option.text = currenciesJSON[currency].name;
+    select_currency.appendChild(option);
+  }
+
+  var td_right = document.createElement('td');
+  td_right.setAttribute('class', 'paddingtd');
+  td_right.appendChild(select_currency);
+
+  var tr = document.createElement('tr');
+  tr.id = 'trCurrency' + count_currencies;
+  tr.appendChild(td_left);
+  tr.appendChild(td_right);
+
+  var container = document.getElementById('currency');
+  container.appendChild(tr);
+
+  changeVisibilityRemove(true);
+
+  if (!init) {
+    localStorage.setItem('currency' + count_currencies, '');
+    count_currencies++;
+    update(0);
+
+    if (count_currencies == MAX_CURRENCIES) changeVisibilityAdd(false);
+  }
+}
