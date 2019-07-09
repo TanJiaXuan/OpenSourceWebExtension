82717f0ff7e8fe852e3b3f1781ec5732ad31a8d5
TanJiaXuan <anjootan@gmail.com>
2019-07-09 11:58:30 +0800

    Update main.js
    word correction

--- Extension_1 (Todolist)/main.js
+++ Extension_1 (Todolist)/main.js
@@ -1,11 +1,12 @@
-var audio = new Audio("alarm.mp3");
+var audio = new Audio('alarm.mp3');
 
 browser.alarms.onAlarm.addListener(alarm => {
   audio.play();
   browser.notifications.create(alarm.name, {
-    type: "basic",
-    iconUrl: "alarm.png",
-    title: "Remainder",
+    type: 'basic',
+    iconUrl: 'alarm.png',
+    title: 'Reminder',
     message: alarm.name
   });
 });
+correc
